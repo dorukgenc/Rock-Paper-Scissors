6f17ceb57772aa4cf4a9f979fa91f622ffd3a8df
Doruk <137659200+dorukgenc@users.noreply.github.com>
2024-03-22 19:26:10 +0300

    Add GUI, Modify the code.

--- script.js
+++ script.js
@@ -1,46 +1,108 @@
+let playCount = 0;
+let computerScore = 0;
+let playerScore = 0;
+
+const screenText = document.querySelector(".screenText");
+document.addEventListener("click", (event) => {
+  let target = event.target;
+
+  switch (target.id) {
+    case "rock":
+      screenText.textContent = playRound("rock", getComputerChoice());
+      break;
+    case "paper":
+      screenText.textContent = playRound("paper", getComputerChoice());
+      break;
+    case "scissor":
+      screenText.textContent = playRound("scissor", getComputerChoice());
+      break;
+  }
+
+  if (playCount === 5) {
+    announceWinner();
+  }
+});
+
 function getComputerChoice() {
   let choices = ["rock", "scissor", "paper"];
   return choices[Math.floor(Math.random() * 3)];
 }
 
+const playerScoreBoard = document.querySelector(".playerScoreBoard");
+const computerScoreBoard = document.querySelector(".computerScoreBoard");
+
+function updatePlayerScore() {
+  playerScore++;
+  playerScoreBoard.textContent = playerScore;
+}
+function updateComputerScore() {
+  computerScore++;
+  computerScoreBoard.textContent = computerScore;
+}
+function updatePlayCount() {
+  playCount++;
+}
+
 function playRound(playerSelection, computerSelection) {
-  let playerSelectionLower = playerSelection.toLowerCase();
-  let computerSelectionLower = computerSelection.toLowerCase();
-  if (playerSelectionLower === "rock") {
-    if (computerSelectionLower === "rock") {
-      return "Try Again!";
-    } else if (computerSelectionLower === "paper") {
-      return "You lost! Paper beats rock.";
+  if (playCount < 5) {
+    let playerSelectionLower = playerSelection.toLowerCase();
+    let computerSelectionLower = computerSelection.toLowerCase();
+    if (playerSelectionLower === "rock") {
+      if (computerSelectionLower === "rock") {
+        updatePlayCount();
+        return "Try Again!";
+      } else if (computerSelectionLower === "paper") {
+        updatePlayCount();
+        updateComputerScore();
+        return "You lost! Paper beats rock.";
+      } else {
+        //computer = scissor
+
+        updatePlayCount();
+        updatePlayerScore();
+        return "You win! Rock beats scissor.";
+      }
+    } else if (playerSelectionLower === "paper") {
+      if (computerSelectionLower === "rock") {
+        updatePlayCount();
+        updatePlayerScore();
+        return "You win! Paper beats rock.";
+      } else if (computerSelectionLower === "paper") {
+        updatePlayCount();
+        return "Try again!";
+      } else {
+        //computer = scissor
+
+        updatePlayCount();
+        updateComputerScore();
+        return "You lost! Scissor beats paper!";
+      }
     } else {
-      //computer = scissor
-      return "You win! Rock beats scissor.";
-    }
-  } else if (playerSelectionLower === "paper") {
-    if (computerSelectionLower === "rock") {
-      return "You win! Paper beats rock.";
-    } else if (computerSelectionLower === "paper") {
-      return "Try again!";
-    } else {
-      //computer = scissor
-      return "You lost! Scissor beats paper!";
-    }
-  } else {
-    //player selection scissors
-    if (computerSelectionLower === "rock") {
-      return "You lost! Rock beats scissor.";
-    } else if (computerSelectionLower === "paper") {
-      return "You win! Scissor beats paper.";
-    } else {
-      //computer = scissor
-      return "Try again!";
+      //player selection scissors
+      if (computerSelectionLower === "rock") {
+        updatePlayCount();
+        updateComputerScore();
+        return "You lost! Rock beats scissor.";
+      } else if (computerSelectionLower === "paper") {
+        updatePlayCount();
+        updatePlayerScore();
+        return "You win! Scissor beats paper.";
+      } else {
+        //computer = scissor
+
+        updatePlayCount();
+        return "Try again!";
+      }
     }
   }
 }
 
-function playGame() {
-  for (var i = 0; i < 5; i++) {
-    let choice = prompt("What is your selection?");
-    console.log(playRound(choice, getComputerChoice()));
+function announceWinner() {
+  if (playerScore > computerScore) {
+    screenText.textContent = "You win!";
+  } else if (playerScore < computerScore) {
+    screenText.textContent = "Computer wins!";
+  } else {
+    screenText.textContent = "Tied!";
   }
 }
-playGame();
